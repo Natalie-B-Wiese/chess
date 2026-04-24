72be950c189a94d4897eda83c27955c0606cab8d
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-04-24 15:55:03 -0400

    Place newly created chess pieces in correct node

--- lib/player_pieces.rb
+++ lib/player_pieces.rb
@@ -15,23 +15,104 @@ class PlayerPieces
   end
 
   def create_pieces(player, board)
-    @pieces.push(King.new(player, board))
-    @pieces.push(Queen.new(player, board))
+    create_king(player, board)
+    create_queen(player, board)
+    create_rooks(player, board)
+    create_bishops(player, board)
+    create_knights(player, board)
+    create_pawns(player, board)
+  end
 
-    2.times do
-      @pieces.push(Rook.new(player, board))
+  private
+
+  def create_king(player, board)
+    mirrored = !player.is_white
+
+    # placement for white player
+    nodes = %w[E1]
+
+    nodes.map! { |id| Grid.mirror_node_id(id) } if mirrored
+
+    nodes.each do |node_id|
+      piece = King.new(player, board)
+      board.node_by_id(node_id).replace_piece(piece)
+      @pieces.push(piece)
     end
+  end
 
-    2.times do
-      @pieces.push(Bishop.new(player, board))
+  def create_queen(player, board)
+    mirrored = !player.is_white
+
+    # placement for white player
+    nodes = %w[D1]
+
+    nodes.map! { |id| Grid.mirror_node_id(id) } if mirrored
+
+    nodes.each do |node_id|
+      piece = Queen.new(player, board)
+      board.node_by_id(node_id).replace_piece(piece)
+      @pieces.push(piece)
     end
+  end
 
-    2.times do
-      @pieces.push(Knight.new(player, board))
+  # creates two rooks and places them in their starting node
+  def create_rooks(player, board)
+    mirrored = !player.is_white
+
+    # placement for white player
+    nodes = %w[A1 H1]
+
+    nodes.map! { |id| Grid.mirror_node_id(id) } if mirrored
+
+    nodes.each do |node_id|
+      piece = Rook.new(player, board)
+      board.node_by_id(node_id).replace_piece(piece)
+      @pieces.push(piece)
     end
+  end
 
-    8.times do
-      @pieces.push(Pawn.new(player, board))
+  def create_bishops(player, board)
+    mirrored = !player.is_white
+
+    # placement for white player
+    nodes = %w[B1 G1]
+
+    nodes.map! { |id| Grid.mirror_node_id(id) } if mirrored
+
+    nodes.each do |node_id|
+      piece = Bishop.new(player, board)
+      board.node_by_id(node_id).replace_piece(piece)
+      @pieces.push(piece)
+    end
+  end
+
+  def create_knights(player, board)
+    mirrored = !player.is_white
+
+    # placement for white player
+    nodes = %w[C1 F1]
+
+    nodes.map! { |id| Grid.mirror_node_id(id) } if mirrored
+
+    nodes.each do |node_id|
+      piece = Knight.new(player, board)
+      board.node_by_id(node_id).replace_piece(piece)
+      @pieces.push(piece)
+    end
+  end
+
+  def create_pawns(player, board)
+    mirrored = !player.is_white
+
+    # placement for white player
+    nodes = %w[A2 B2 C2 D2 E2 F2 G2 H2]
+
+    nodes.map! { |id| Grid.mirror_node_id(id) } if mirrored
+
+    nodes.each do |node_id|
+      piece = Pawn.new(player, board)
+      board.node_by_id(node_id).replace_piece(piece)
+      @pieces.push(piece)
     end
   end
 end
