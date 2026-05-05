9b6f597b5d62db2464890d9d8bba8bf8979552e4
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-05-05 14:44:39 -0400

    Implement movement for Queen

--- lib/piece/queen.rb
+++ lib/piece/queen.rb
@@ -1,11 +1,44 @@
 # frozen-string-literal: true
 
 require_relative 'piece'
+require_relative 'moveset/diagonal_sliding_movement'
+require_relative 'moveset/horizontal_sliding_movement'
+require_relative 'moveset/vertical_sliding_movement'
 
 # a queen piece
 class Queen < Piece
+  include DiagonalSlidingMovement
+  include HorizontalSlidingMovement
+  include VerticalSlidingMovement
+
   def initialize(player, board)
     graphic = 'Q'
     super(player, board, graphic)
+  end
+
+  def paths
+    valid_diagonal_nodes + valid_vertical_nodes + valid_horizontal_nodes
+  end
+
+  private
+
+  def valid_diagonal_nodes
+    start_node = node
+    trim_friendly_endpoint(up_right_nodes(@board, start_node), start_node) +
+      trim_friendly_endpoint(up_left_nodes(@board, start_node), start_node) +
+      trim_friendly_endpoint(down_left_nodes(@board, start_node), start_node) +
+      trim_friendly_endpoint(down_right_nodes(@board, start_node), start_node)
+  end
+
+  def valid_vertical_nodes
+    start_node = node
+    trim_friendly_endpoint(vertical_nodes(@board, start_node, 1), start_node) +
+      trim_friendly_endpoint(vertical_nodes(@board, start_node, -1), start_node)
+  end
+
+  def valid_horizontal_nodes
+    start_node = node
+    trim_friendly_endpoint(horizontal_nodes(@board, start_node, 1), start_node) +
+      trim_friendly_endpoint(horizontal_nodes(@board, start_node, -1), start_node)
   end
 end
