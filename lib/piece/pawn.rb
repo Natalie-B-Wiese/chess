e7729a6182350660e1f2a5b7c4e155a9d36274a6
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-05-05 15:45:09 -0400

    Implement ability for pawn to capture opponent
    Pawn can now move diagonal if there is an opponent piece on that diagonal position.

--- lib/piece/pawn.rb
+++ lib/piece/pawn.rb
@@ -2,10 +2,12 @@
 
 require_relative 'piece'
 require_relative 'moveset/vertical_sliding_movement'
+require_relative 'moveset/diagonal_sliding_movement'
 
 # a pawn piece
 class Pawn < Piece
   include VerticalSlidingMovement
+  include DiagonalSlidingMovement
 
   def initialize(player, board)
     graphic = 'P'
@@ -17,7 +19,7 @@ class Pawn < Piece
   # paths
   # Returns an array of all valid nodes that this piece could move to
   def paths
-    valid_vertical_nodes
+    valid_vertical_nodes + valid_diagonal_nodes
   end
 
   private
@@ -38,5 +40,22 @@ class Pawn < Piece
     path_array.pop if path_array.last.full?
 
     path_array
+  end
+
+  # gets nodes that are diagonal to this pawn and are occuppied by an opposing piece
+  def valid_diagonal_nodes
+    start_node = node
+    diagonal_nodes.reject do |n|
+      n.nil? || !n.full? || n.same_player?(start_node)
+    end
+  end
+
+  def diagonal_nodes
+    start_node = node
+    if @forward == 1
+      up_right_nodes(@board, start_node, 1) + up_left_nodes(@board, start_node, 1)
+    else
+      down_right_nodes(@board, start_node, 1) + down_left_nodes(@board, start_node, 1)
+    end
   end
 end
