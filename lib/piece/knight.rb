101fab5611408e50e965d588da2b66c88cea5c1d
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-05-05 15:40:06 -0400

    Implement Knight movements
    Knight can now move around and follows rules

--- lib/piece/knight.rb
+++ lib/piece/knight.rb
@@ -9,4 +9,31 @@ class Knight < Piece
 
     super(player, board, graphic)
   end
+
+  def paths
+    start_node = node
+    move_nodes(start_node).reject do |n|
+      n.nil? || n.same_player?(start_node)
+    end
+  end
+
+  private
+
+  # an array of nodes that this knight could possibly move to. Might include some nil nodes if they are out of bounds
+  def move_nodes(start_node)
+    row = start_node.row
+    column = start_node.column
+
+    [
+      @board.node_at_row_column(row + 1, column + 2),
+      @board.node_at_row_column(row - 1, column + 2),
+      @board.node_at_row_column(row + 2, column + 1),
+      @board.node_at_row_column(row - 2, column + 1),
+
+      @board.node_at_row_column(row + 1, column - 2),
+      @board.node_at_row_column(row - 1, column - 2),
+      @board.node_at_row_column(row + 2, column - 1),
+      @board.node_at_row_column(row - 2, column - 1)
+    ]
+  end
 end
