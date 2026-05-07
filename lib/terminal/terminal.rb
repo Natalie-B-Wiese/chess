9aa9ed991ed008d738b4b50385f372ccf557aa89
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-05-07 13:27:50 -0400

    Implement draw text box in terminal method
    Now a text box with a header and content can easily be created in the terminal

--- lib/terminal/terminal.rb
+++ lib/terminal/terminal.rb
@@ -4,7 +4,44 @@ require_relative 'terminal_colors'
 
 # for printing stuff to the terminal
 module Terminal
+  CORNER = '+'
+  V_EDGE = '-'
+  H_EDGE = '|'
+
   def self.print_error(str)
     puts "#{TerminalColors::RED}#{str}#{TerminalColors::RESET}"
+  end
+
+  def self.create_info_box(header, content_array, width)
+    draw_header_box(header, width)
+
+    # decrease width by 2 to ignore the H_EDGE on either side
+    width -= 2
+
+    content_array.each do |line|
+      puts H_EDGE + padded_string(line, width) + H_EDGE
+    end
+
+    puts CORNER + (V_EDGE * width) + CORNER
+  end
+
+  def self.draw_header_box(header, width)
+    # decrease width by 2 to ignore the H_EDGE on either side
+    width -= 2
+
+    puts CORNER + (V_EDGE * width) + CORNER
+    puts H_EDGE + centered_string(header, width) + H_EDGE
+    puts CORNER + (V_EDGE * width) + CORNER
+  end
+
+  # returns a string that is horizontally centered
+  def self.centered_string(string, width)
+    padding_left = (width / 2) - (string.length / 2)
+    padded_string(string, width, padding_left)
+  end
+
+  # pads the string a specified amount on the left, and pads it the remaining amount on the right
+  def self.padded_string(string, width, left_padding = 1)
+    (' ' * left_padding) + string + (' ' * (width - string.length - left_padding))
   end
 end
--- main.rb
+++ main.rb
@@ -2,18 +2,25 @@ require_relative 'lib/game'
 require_relative 'lib/player'
 require_relative 'lib/player_pieces'
 
+require_relative 'lib/terminal/terminal'
+
 # create a game, players, and play the game
 player1 = Player.new('Player 1', true)
 player2 = Player.new('Player 2', false)
 
-puts "\e[31mThis is red\e[0m"
 game = Game.new(player1, player2)
 
 player1_pieces = PlayerPieces.new(player1, game.board)
 player2_pieces = PlayerPieces.new(player2, game.board)
 
-puts game.board.node_by_piece_type(King)
+box_info = [
+  'HELP - shows this help box',
+  'PIECES - shows full names of pieces',
+  'MOVES - show all available moves',
+  'QUIT - exits the game without saving',
+  'SAVE - save the games as a file'
+]
 
-puts game.board.nodes_with_player_piece(player1)
+Terminal.create_info_box('HELP:', box_info, 40)
 
 game.play_game
